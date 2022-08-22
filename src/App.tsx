7108aefb8481e5aab1a35856c988d3c1de2229c0
Ime <imeekwere15@gmail.com>
2022-08-22 13:55:05 +0100

    hmm

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ime
+          Learn React ime new
         </a>
       </header>
     </div>
